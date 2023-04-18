a9fbfa360e49db7a41217a095244571cce427ab4
Jason Li <124201063+Jleeluit@users.noreply.github.com>
2023-04-18 17:32:54 -0400

    Update nameandcolor.py

--- nameandcolor.py
+++ nameandcolor.py
@@ -1,3 +1,7 @@
-"""
-Your module description
-"""
+# Get user input
+name = input("What is your name? ")
+age = input("What is your favrotie color? ")
+
+# Display greeting
+print(f"Hello, {name}!")
+print(f"You favorite color is {color}.")
